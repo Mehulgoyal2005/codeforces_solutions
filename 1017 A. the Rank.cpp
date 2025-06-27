a29bb5903e0b19cf1eed52924be9038b8bff2da7
Mehul <mehul63755@gamil.com>
2025-06-27 07:37:41 +0530

    added

--- 1017 A. the Rank.cpp
+++ 1017 A. the Rank.cpp
@@ -3,11 +3,9 @@ using namespace std;
 int main()
 {
     int n,i=0;
-    
     int a,b,c,d;
     int s[1001];
     cin>>n;
-
     for(i=0; i<n; i++){
         int sum=0;
         cin>>a>>b>>c>>d;
@@ -20,8 +18,9 @@ int main()
     sort( s ,s+n , greater<int>() );
     for(int i=0; i<n; i++)
     {
-        if(  sm == s [i]){
-
+        if(  sm == s [i])
+        {
+            
             cout<<i+1<<endl;
             break;
         }
